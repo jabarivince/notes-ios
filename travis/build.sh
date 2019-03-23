ed8b47d44a0b414d464126d43b4447a86310cbe9
Jabari Dash <jabari.dash@gmail.com>
2019-03-23 16:25:08 -0500

    ios10

--- travis/build.sh
+++ travis/build.sh
@@ -1,3 +1,3 @@
 set -e
 
-set -o pipefail && xcodebuild -project notes.xcodeproj -scheme notesTests -destination platform\=iOS\ Simulator,OS\=12.1,name\=iPhone\ X build test | xcpretty
+set -o pipefail && xcodebuild -project notes.xcodeproj -scheme notesTests -destination platform\=iOS\ Simulator,OS\=10.0,name\=iPhone\ X build test | xcpretty

02c74cd78e3b7d8679b31bf54a951b564da5e399
Jabari Dash <jabari.dash@gmail.com>
2019-03-23 16:29:48 -0500

    moved to src

--- travis/build.sh
+++ travis/build.sh
@@ -1,3 +1,5 @@
+cd ${TRAVIS_BUILD_DIR}/src
+
 set -e
 
-set -o pipefail && xcodebuild -project notes.xcodeproj -scheme notesTests -destination platform\=iOS\ Simulator,OS\=10.0,name\=iPhone\ X build test | xcpretty
+set -o pipefail && xcodebuild -project notes.xcodeproj -scheme notesTests -destination platform\=iOS\ Simulator,OS\=10.0,name\=iPhone\ 6 build test | xcpretty

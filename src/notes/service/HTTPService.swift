b2fffb759ae7964f4ed86c27789340acb6b5ab79
Jabari Dash <jabari.dash@gmail.com>
2019-03-31 22:48:24 -0500

    went with cocoapods instead of carthage

---
+++ src/notes/service/HTTPService.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPService.swift
+//  notes
+//
+//  Created by jabari on 3/31/19.
+//  Copyright © 2019 jabari. All rights reserved.
+//
+
+import Alamofire
+
+protocol HTTPService {}
+
+class DefaultHTTPService: HTTPService {}
+
+

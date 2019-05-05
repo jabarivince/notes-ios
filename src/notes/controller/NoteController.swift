12b0f3bfbc60b480af4984f38eb21d1b98b1c569
Jabari Dash <jabari.dash@gmail.com>
2019-05-05 01:06:34 -0500

    implemented auto save and auto resize

--- src/notes/controller/NoteController.swift
+++ src/notes/controller/NoteController.swift
@@ -9,18 +9,14 @@
 import UIKit
 
 class NoteController: UIViewController {
-    private let textView: UITextView
-    private var trashButton: UIBarButtonItem!
-    private var spacer: UIBarButtonItem!
-    private var noteTitle = UIButton(type: .custom)
-    
-    private let note: Note
+    private let textView = UITextView()
     private let noteService: NoteService
+    private let note: Note
+    private var timer: Timer? = nil
     
     init(note: Note, noteService: NoteService) {
         self.note = note
         self.noteService = noteService
-        self.textView = UITextView()
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -30,50 +26,46 @@ class NoteController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        textView.frame.size.width = view.bounds.width
+        textView.frame.size.width  = view.bounds.width
         textView.frame.size.height = view.bounds.height
-        
-       // view.pinSubview(textView)
     }
     
     override func viewDidLoad() {
-        // set note title
-        noteTitle.frame = CGRect(x: 0, y: 0, width: 100, height: 40)
-        noteTitle.backgroundColor = .clear
-        noteTitle.setTitle(note.title, for: .normal)
+        let noteTitle              = UIButton(type: .custom)
+        noteTitle.frame            = CGRect(x: 0, y: 0, width: 100, height: 40)
+        noteTitle.backgroundColor  = .clear
         noteTitle.titleLabel?.font = noteTitle.titleLabel?.font.bolded
+        noteTitle.setTitle(note.title, for: .normal)
         noteTitle.setTitleColor(.black, for: .normal)
         noteTitle.addTarget(self, action: #selector(changeNoteName), for: .touchUpInside)
         navigationItem.titleView = noteTitle
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(sendNote))
-        view.translatesAutoresizingMaskIntoConstraints = true
-        textView.translatesAutoresizingMaskIntoConstraints = true
-        textView.text = note.body
-        textView.delegate = self
-        view.addSubview(textView)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action,
+                                                            target: self,
+                                                            action: #selector(sendNote))
         
-        spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
-        trashButton = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteNote))
+        let spacer            = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let trashButton       = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteNote))
         trashButton.tintColor = .red
-        toolbarItems = [spacer, trashButton]
+        toolbarItems          = [spacer, trashButton]
         navigationController?.setToolbarHidden(false, animated: false)
         
-        let toolbar:UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0,  width: self.view.frame.size.width, height: 30))
-        let flexSpace = UIBarButtonItem(barButtonSystemItem:    .flexibleSpace, target: nil, action: nil)
-        let doneBtn: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(closeKeyboard))
+        let toolbar   = UIToolbar(frame: CGRect(x: 0, y: 0,  width: self.view.frame.size.width, height: 30))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneBtn   = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(closeKeyboard))
         toolbar.setItems([flexSpace, doneBtn], animated: false)
         toolbar.sizeToFit()
-        textView.inputAccessoryView = toolbar
         
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardDidHideNotification, object: nil)
+        textView.text               = note.body
+        textView.delegate           = self
+        textView.inputAccessoryView = toolbar
+        view.addSubview(textView)
+        addObservers()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.navigationBar.prefersLargeTitles = false
-        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -107,7 +99,7 @@ extension NoteController {
         promptYesOrNo(withMessage: message, onYes: onYes, onNo: nil)
     }
     
-    private func saveNote() {
+    @objc private func saveNote() {
         note.body = textView.text
         noteService.saveNote(note: note)
     }
@@ -117,7 +109,7 @@ extension NoteController {
     }
     
     @objc private func changeNoteName() {
-            let message = "Rename your note"
+            let message     = "Rename your note"
             let placeholder = "Untitled"
             
             func onConfirm(title: String?) {
@@ -132,26 +124,60 @@ extension NoteController {
     }
 }
 
-extension NoteController: UITextViewDelegate {
-    func textViewDidChange(_ textView: UITextView) {
-        print("HERE")
+extension NoteController {
+    private func addObservers() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
     }
     
-    private func getKeyboardHeight(from notification: Notification) -> CGFloat {
-        return ((notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue ?? .zero).height
+    /// Returns size frame of the keyboard
+    private func getKeyboardSize(from notification: Notification) -> CGRect? {
+        return (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue
     }
     
+    /// Shrinks scrollview height such that keyboard does not cover bottom
     @objc private func keyboardWillShow(notification: Notification) {
-        if view.frame.origin.y != 0 {
-            var frame = view.frame
-            frame.origin.y -= getKeyboardHeight(from: notification)
-            view.frame = frame
-        }
+        guard let keyboardSize = getKeyboardSize(from: notification) else { return }
+        
+        let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        var frame         = view.frame
+        
+        textView.contentInset          = contentInsets
+        textView.scrollIndicatorInsets = contentInsets
+        frame.size.height             -= keyboardSize.height
     }
     
+    /// Restores scrollView height to full height
     @objc private func keyboardWillHide(notification: Notification) {
-        if view.frame.origin.y != 0 {
-            view.frame.origin.y += getKeyboardHeight(from: notification)
+        UIView.animate(withDuration: 0.2) { [weak self] in
+            guard let self = self else { return }
+            let contentInsets                   = UIEdgeInsets.zero
+            self.textView.contentInset          = contentInsets
+            self.textView.scrollIndicatorInsets = contentInsets
         }
+    }
+}
+
+extension NoteController: UITextViewDelegate {
+    
+    /// Autosave half a second after stop typing
+    func textViewDidChange(_ textView: UITextView) {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 0.5,
+                                     target: self,
+                                     selector: #selector(saveNote),
+                                     userInfo: nil,
+                                     repeats: false)
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        saveNote()
     }
 }

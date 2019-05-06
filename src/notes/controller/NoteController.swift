6d8c0003ca256525c611c9e82735e86753975033
Jabari Dash <jabari.dash@gmail.com>
2019-05-05 21:41:22 -0500

    merging all the notecontroller changes

--- src/notes/controller/NoteController.swift
+++ src/notes/controller/NoteController.swift
@@ -9,17 +9,15 @@
 import UIKit
 
 class NoteController: UIViewController {
-    private var textView: UITextView!
-    private var trashButton: UIBarButtonItem!
-    private var spacer: UIBarButtonItem!
-    private var noteTitle = UIButton(type: .custom)
-    
-    private let note: Note
+    private let textView: UITextView
     private let noteService: NoteService
+    private let note: Note
+    private var timer: Timer?
     
     init(note: Note, noteService: NoteService) {
-        self.note = note
+        self.note        = note
         self.noteService = noteService
+        self.textView    = UITextView()
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -29,51 +27,49 @@ class NoteController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
-        textView.frame.size.width = view.bounds.width
+        textView.frame.size.width  = view.bounds.width
         textView.frame.size.height = view.bounds.height
     }
     
     override func viewDidLoad() {
-        // set note title
-        noteTitle.frame = CGRect(x: 0, y: 0, width: 100, height: 40)
-        noteTitle.backgroundColor = .clear
-        noteTitle.setTitle(note.title, for: .normal)
+        let noteTitle              = UIButton(type: .custom)
+        noteTitle.frame            = CGRect(x: 0, y: 0, width: 100, height: 40)
+        noteTitle.backgroundColor  = .clear
         noteTitle.titleLabel?.font = noteTitle.titleLabel?.font.bolded
+        noteTitle.setTitle(note.title, for: .normal)
         noteTitle.setTitleColor(.black, for: .normal)
         noteTitle.addTarget(self, action: #selector(changeNoteName), for: .touchUpInside)
         
-        navigationItem.titleView = noteTitle
+        navigationItem.titleView          = noteTitle
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(sendNote))
         
-        spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
-        trashButton = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteNote))
+        let spacer            = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let trashButton       = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteNote))
+        toolbarItems          = [spacer, trashButton]
         trashButton.tintColor = .red
-        toolbarItems = [spacer, trashButton]
         navigationController?.setToolbarHidden(false, animated: false)
         
-        let toolbar:UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0,  width: self.view.frame.size.width, height: 30))
-        let flexSpace = UIBarButtonItem(barButtonSystemItem:    .flexibleSpace, target: nil, action: nil)
-        let doneBtn: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(closeKeyboard))
+        let toolbar   = UIToolbar(frame: CGRect(x: 0, y: 0,  width: self.view.frame.size.width, height: 30))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneBtn   = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(closeKeyboard))
         toolbar.setItems([flexSpace, doneBtn], animated: false)
         toolbar.sizeToFit()
         
-        textView = UITextView()
-        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.font                              = .preferredFont(forTextStyle: .body)
+        textView.text                              = note.body
+        textView.delegate                          = self
+        textView.isEditable                        = false
+        textView.dataDetectorTypes                 = .all
+        textView.inputAccessoryView                = toolbar
         textView.adjustsFontForContentSizeCategory = true
-        textView.font = .preferredFont(forTextStyle: .body)
-        textView.text = note.body
-        textView.delegate = self
-        textView.isEditable = false
-        textView.dataDetectorTypes = .all
-        textView.inputAccessoryView = toolbar
         view.addSubview(textView)
         
+        addObservers()
         configureTagGesture()
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated) 
+        super.viewWillAppear(animated)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -103,11 +99,11 @@ extension NoteController {
             noteService.deleteNote(note: note)
             closeNote(withoutSaving: true)
         }
-
+        
         promptYesOrNo(withMessage: message, onYes: onYes, onNo: nil)
     }
     
-    private func saveNote() {
+    @objc private func saveNote() {
         note.body = textView.text
         noteService.saveNote(note: note)
     }
@@ -117,22 +113,63 @@ extension NoteController {
     }
     
     @objc private func changeNoteName() {
-            let message = "Rename your note"
-            let placeholder = "Untitled"
-            
-            func onConfirm(title: String?) {
-                note.title = title
-                self.viewDidLoad()
-            }
-            
-            promptForText(withMessage: message,
-                          placeholder: placeholder,
-                          onConfirm: onConfirm,
-                          onCancel: nil)
+        let message     = "Rename your note"
+        let placeholder = "Untitled"
+        
+        func onConfirm(title: String?) {
+            note.title = title
+            self.viewDidLoad()
+        }
+        
+        promptForText(withMessage: message,
+                      placeholder: placeholder,
+                      onConfirm: onConfirm,
+                      onCancel: nil)
+    }
+}
+
+extension NoteController {
+    private func addObservers() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    /// Returns size frame of the keyboard
+    private func getKeyboardSize(from notification: Notification) -> CGRect? {
+        return (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue
+    }
+    
+    /// Shrinks scrollview height such that keyboard does not cover bottom
+    @objc private func keyboardWillShow(notification: Notification) {
+        guard let keyboardSize = getKeyboardSize(from: notification) else { return }
+        
+        let contentInsets              = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        var frame                      = view.frame
+        textView.contentInset          = contentInsets
+        textView.scrollIndicatorInsets = contentInsets
+        frame.size.height             -= keyboardSize.height
+    }
+    
+    /// Restores scrollView height to full height
+    @objc private func keyboardWillHide(notification: Notification) {
+        UIView.animate(withDuration: 0.2) { [weak self] in
+            guard let self = self else { return }
+            let contentInsets                   = UIEdgeInsets.zero
+            self.textView.contentInset          = contentInsets
+            self.textView.scrollIndicatorInsets = contentInsets
+        }
     }
 }
 
 extension NoteController: UIGestureRecognizerDelegate {
+    /// Configures tag gesture on textView
     private func configureTagGesture() {
         let recognizer = UITapGestureRecognizer()
         recognizer.delegate = self
@@ -141,14 +178,27 @@ extension NoteController: UIGestureRecognizerDelegate {
         textView.addGestureRecognizer(recognizer)
     }
     
-    @objc private func textViewTapped(_ recognizer: UIGestureRecognizer) {
+    /// Enables editing on textView and displays keyboard
+    @objc private func textViewTapped() {
         textView.isEditable = true
         textView.becomeFirstResponder()
     }
 }
 
 extension NoteController: UITextViewDelegate {
+    /// Autosaves half a second after stop typing
+    func textViewDidChange(_ textView: UITextView) {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 0.5,
+                                     target: self,
+                                     selector: #selector(saveNote),
+                                     userInfo: nil,
+                                     repeats: false)
+    }
+    
+    /// Disabled editing (to re-anble hyperlink detection, etc) and save
     func textViewDidEndEditing(_ textView: UITextView) {
         textView.isEditable = false
+        saveNote()
     }
 }

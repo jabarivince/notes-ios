5ef3424c5e6b0e8d198e303e4804a754a6b9db1d
Jabari Dash <jabari.dash@gmail.com>
2019-05-09 02:21:04 -0500

    added cta, need to find attributed string

--- src/notes/view/NoteListBackgroundView.swift
+++ src/notes/view/NoteListBackgroundView.swift
@@ -9,30 +9,92 @@
 import UIKit
 
 class NoteListBackgroundView: UIView {
-    private let label: UILabel
+    enum State: String {
+        case hiddenState           = ""
+        case noNotesFoundState     = "No search results found"
+        case noNotesAvailableState = "Click + to create a new note"
+    }
+    
+    private let label = UILabel()
     
     var state: State? = .hiddenState {
         didSet {
-            label.text = state?.rawValue
+            guard let state = state else { return }
+            
+            switch state {
+            case .noNotesAvailableState:
+                let text     = state.rawValue
+                let attrText = NSMutableAttributedString(string: text)
+                let range    = (text as NSString).range(of: "+")
+                let color    = tintColor ?? UIColor(red: 0, green: 122/255, blue: 1, alpha: 1)
+                
+                attrText.addAttribute(.foregroundColor, value: color, range: range)
+                label.attributedText = attrText
+                
+            case .noNotesFoundState, .hiddenState:
+                callback = nil
+                fallthrough
+            default:
+                label.text = state.rawValue
+            }
         }
     }
     
+    var callback: (() -> Void)?
+    
     override init(frame: CGRect) {
-        label               = UILabel(frame: frame)
-        label.textColor     = .darkGray
-        label.textAlignment = .center
-        label.adjustsFontForContentSizeCategory = true
         super.init(frame: frame)
-        addSubview(label)
+        setupSubviews()
+        setupLabel()
+        setupTapGestureRecognizer()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+private extension NoteListBackgroundView {
+    func setupSubviews() {
+        let view = UIView(frame: frame)
+        view.addSubview(label)
+        addSubview(view)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
     
-    enum State: String {
-        case hiddenState           = ""
-        case noNotesFoundState     = "No notes found"
-        case noNotesAvailableState = "Click + to create a new note"
+    func setupLabel() {
+        label.textColor                                 = .darkGray
+        label.textAlignment                             = .center
+        label.isUserInteractionEnabled                  = true
+        label.adjustsFontForContentSizeCategory         = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func setupTapGestureRecognizer() {
+        let recognizer                  = UITapGestureRecognizer(target: nil, action: nil)
+        recognizer.delegate             = self
+        recognizer.numberOfTapsRequired = 1
+        recognizer.addTarget(self, action: #selector(labelTapped))
+        label.addGestureRecognizer(recognizer)
+    }
+}
+
+
+extension NoteListBackgroundView: UIGestureRecognizerDelegate {
+    @objc private func labelTapped(_ recognizer: UITapGestureRecognizer) {
+        if attributedTextTapped(recognizer) {
+            callback?()
+        }
+    }
+    
+    private func attributedTextTapped(_ recognizer: UITapGestureRecognizer) -> Bool {
+        guard state == .noNotesAvailableState else { return false }
+        
+        // TODO: Implement logic to determine if attributed text was clicked
+        return true
     }
 }

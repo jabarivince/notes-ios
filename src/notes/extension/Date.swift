e8486a3652ae9c6f2805bbed2981487019cc21db
Jabari Dash <jabari.dash@gmail.com>
2019-05-10 00:14:14 -0500

    date formating

--- src/notes/extension/Date.swift
+++ src/notes/extension/Date.swift
@@ -10,11 +10,49 @@ import Foundation
 
 extension Date {
     var formatted: String {
-        let formatter        = DateFormatter()
-        formatter.locale     = Locale(identifier: "en_US_POSIX")
-        formatter.dateFormat = "h:mm a 'on' MMMM dd, yyyy"
-        formatter.amSymbol   = "AM"
-        formatter.pmSymbol   = "PM"
+        let formatter      = DateFormatter()
+        formatter.locale   = Locale(identifier: "en_US_POSIX")
+        formatter.amSymbol = "AM"
+        formatter.pmSymbol = "PM"
+        
+        if isToday {
+            formatter.dateFormat = Date.today
+        } else if isThisWeek {
+            formatter.dateFormat = Date.thisWeek
+        } else {
+            formatter.dateFormat = Date.beyondLastWeek
+        }
+        
         return formatter.string(from: self)
+    }
+}
+
+private extension Date {
+    var today: Date {
+        return Date()
+    }
+    
+    var isToday: Bool {
+        return daysFromToday == 0
+    }
+    
+    var isThisWeek: Bool {
+        return daysFromToday <= 6
+    }
+    
+    var daysFromToday: Int {
+        return Calendar.current.dateComponents([.day], from: self, to: today).day!
+    }
+    
+    static var today: String {
+        return "h:mm a"
+    }
+    
+    static var thisWeek: String {
+        return "EEEE"
+    }
+    
+    static var beyondLastWeek: String {
+        return "h:mm a 'on' MMMM dd, yyyy"
     }
 }
